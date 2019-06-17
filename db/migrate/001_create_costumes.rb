dbed3d790f52eda812f8552a44aafa16b1d6d3b7
Dennis <dennis.basyrov@gmail.com>
2019-06-17 20:30:20 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,9 +13,10 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :price
+      t.integer :price
       t.integer :size
       t.string :image_url
+      
     end
   end
   
