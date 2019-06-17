976d3590e3833027e46101b5803b24bcee342c90
Dennis <dennis.basyrov@gmail.com>
2019-06-17 20:29:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,4 +9,14 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+  
 end   
