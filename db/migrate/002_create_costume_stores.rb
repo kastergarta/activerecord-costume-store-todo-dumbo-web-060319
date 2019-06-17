db1e36cf44e6998ce721699d7aa32b7d12e53d2e
Dennis <dennis.basyrov@gmail.com>
2019-06-17 20:46:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
